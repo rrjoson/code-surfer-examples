7080681bd8e88c542d2dfaabf1d06abb4a563012
rrjoson <rrjoson08@gmail.com>
2019-07-13 09:29:34 +0800

    Initial commit

---
+++ src/greeting-loader.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const load = require.context("./", true, /greeting\..*js$/);
+
+export default function GreetingLoader({
+  version = "1.1",
+  theme,
+}) {
+  const Greeting = load("./greeting." + version + ".js")
+    .default;
+
+  return (
+    <div
+      style={{
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background:
+          theme && theme.codeSurfer.code.background,
+      }}
+    >
+      <Greeting name="Mary" />
+    </div>
+  );
+}
